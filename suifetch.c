e6c37f94f6458e03657faf8c4fc509535ce62d15
Birdabo404 <birdabo420@gmail.com>
2025-06-02 23:58:50 +0800

    added to-do | dont forget

--- suifetch.c
+++ suifetch.c
@@ -128,19 +128,10 @@ int main() {
     }
 
     get_uptime(); // temporary lol 
-
-    /*
-    wtf is this.
-    boottime=`sysctl -n kern.boottime | awk '{print $4}' | sed 's/,//g'`
-    unixtime=`date +%s`
-    timeAgo=$(($unixtime - $boottime))
-    uptime=`awk -v time=$timeAgo 'BEGIN { seconds = time % 60; minutes = int(time / 60 % 60); hours = int(time / 60 / 60 % 24); days = int(time / 60 / 60 / 24); printf("%.0f days, %.0f hours, %.0f minutes, %.0f seconds", days, hours, minutes, seconds); exit }'`
-    echo $uptime
-
-    someone help me convert this to up-time T-T
-    */
-
     return 0;
 
 }
+
+//to do: refactor code to full work back in linux; currently its working fine for macOS
+
 
